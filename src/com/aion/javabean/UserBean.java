e80b30a766652e394851220e4c0ee95f5328d473
Paulo <pncorpuz@gmail.com>
2018-03-07 00:40:13 +0800

    javabean

---
+++ src/com/aion/javabean/UserBean.java
@@ -0,0 +1,41 @@
+package com.aion.javabean;
+
+import java.io.Serializable;
+
+public class UserBean implements Serializable {
+	
+	public static final String USERID = "iduser";
+	public static final String USERNAME = "username";
+	public static final String PASSWORD ="password";
+	public static final String PRIVILEGE = "previlege";
+	
+	private int iduser;
+	private  String username;
+	private String password;
+	private int previlege;
+	public int getIduser() {
+		return iduser;
+	}
+	public void setIduser(int iduser) {
+		this.iduser = iduser;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getPrevilege() {
+		return previlege;
+	}
+	public void setPrevilege(int previlege) {
+		this.previlege = previlege;
+	} 
+
+}
