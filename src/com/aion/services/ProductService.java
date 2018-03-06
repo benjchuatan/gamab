16d67981f31588e601b946c1187be6229a264247
erccool <ecurugan12@gmail.com>
2018-03-07 05:43:53 +0800

    try

--- src/com/aion/services/ProductService.java
+++ src/com/aion/services/ProductService.java
@@ -14,10 +14,48 @@ import com.aion.db.DBManager;
 import com.aion.javabean.Product;
 
 
+
 public class ProductService {
 	String url ="jdbc:mysql://localhost:3306/secprg";
 	String username ="root";
 	String password = "password";
+	
+	public ArrayList getSelectedProducts(Product p) {
+		ArrayList<Product> productlists = new ArrayList<>();		
+		String sql = "SELECT * FROM secprg." + Product.TABLE_NAME + " WHERE " + 
+				Product.NAME + " LIKE" + " ? ";
+		String what;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url,username,password);
+			what = "%" + p.getName() +"%"; 
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, what);
+			ResultSet rs = st.executeQuery();
+			
+			
+			while(rs.next()) {
+				Product ap = new Product();
+				
+				ap.setName(rs.getString(Product.NAME));
+				ap.setManufacturer(rs.getString(Product.MANUFACTURER));
+				ap.setDescription(rs.getString(Product.DESCRIPTION));
+				ap.setIdproducts(rs.getInt(Product.IDPRODUCTS));
+				ap.setPrice(rs.getFloat(Product.PRICE));
+				ap.setFilename(rs.getString(Product.FILENAME));
+				System.out.println("asdasdas " + ap.getFilename());
+
+				productlists.add(ap);
+			}		
+
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return productlists;
+	}
 	
 	public ArrayList getAllProducts(){
 		ArrayList<Product> productlists = new ArrayList<>();
---
+++ src/com/aion/servlets/SearchProductServlet.java
@@ -0,0 +1,53 @@
+package com.aion.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.aion.services.ProductService;
+import com.aion.javabean.Product;
+
+/**
+ * Servlet implementation class SearchProductServlet
+ */
+@WebServlet("/SearchProductServlet")
+public class SearchProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchProductServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		Product p = new Product();
+		p.setName(request.getParameter(Product.NAME));
+		ProductService productService = new ProductService();
+		ArrayList<Product> Product = productService.getSelectedProducts(p);
+
+		request.setAttribute("productlists", Product);
+
+	}
+
+}
