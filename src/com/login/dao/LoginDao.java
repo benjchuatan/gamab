81fcab133cec41e99564df1e735319e1a72db7b2
Benjamin Tan <benjtan143@gmail.com>
2018-03-06 16:19:01 +0800

    gamab number 1
    new repo

---
+++ src/com/login/dao/LoginDao.java
@@ -0,0 +1,40 @@
+package com.login.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginDao {
+	
+	String sql = "select * from user where username=? and password=?";
+	String url ="jdbc:mysql://localhost:3306/secprg";
+	String username ="root";
+	String password = "password";
+	
+	public boolean check(String uname, String pass) throws SQLException {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url,username,password);
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, uname);
+			st.setString(2, pass);
+			ResultSet rs = st.executeQuery();
+			
+			if(rs.next()) {
+				return true;
+			}
+			
+			
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+
+
+}
