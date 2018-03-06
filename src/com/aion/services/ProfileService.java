cd40e635c968dae0256f4ee8605be3cce7c8205b
Paulo <pncorpuz@gmail.com>
2018-03-07 01:19:55 +0800

    profile service

--- src/com/aion/services/ProfileService.java
+++ src/com/aion/services/ProfileService.java
@@ -1,5 +1,17 @@
 package com.aion.services;
 
+import java.util.ArrayList;
+
+import com.aion.javabean.UserBean;
+
 public class ProfileService {
 
+	public ArrayList getSelectedUser(UserBean a){
+		ArrayList<UserBean> userbeanlists = new ArrayList<>();
+		String sql = "SELECT * FROM " + UserBean.TABLE_NAME + " WHERE " + 
+
+		Artworks.NAME + " LIKE" + " ? ";
+		
+		return userbeanlists;
+	}
 }
