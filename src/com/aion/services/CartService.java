66d8017c7edf6e9fdbb0c869252c10f83aeff277
Paolo Gamab <pgcgamab@gmail.com>
2018-03-14 02:20:14 +0800

    asd

--- src/com/aion/services/CartService.java
+++ src/com/aion/services/CartService.java
@@ -2,8 +2,12 @@ package com.aion.services;
 
 public class CartService {
  
+	String url ="jdbc:mysql://localhost:3306/secprg";
+	String username ="root";
+	String password = "password";
+	
 	public void addcart() {
-		
+		String sql = "Insert into cart(iduser,idproduct) values(?,?)";
 		
 	}
 	
