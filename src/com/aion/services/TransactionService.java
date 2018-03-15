b191a3a019482fc0b32f0e5985579ffd928475b5
erccool <ecurugan12@gmail.com>
2018-03-15 12:02:47 +0800

    new table
    new table

--- src/com/aion/services/TransactionService.java
+++ src/com/aion/services/TransactionService.java
@@ -3,8 +3,11 @@ package com.aion.services;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import com.aion.javabean.Product;
 import com.aion.javabean.TransBean;
 import com.aion.javabean.TransBeana;
 
@@ -13,6 +16,39 @@ public class TransactionService {
 	String url ="jdbc:mysql://localhost:3306/secprg";
 	String username ="root";
 	String password = "password";
+	
+	public ArrayList getAllTransactions(int id) {		
+		ArrayList<Product> productlists = new ArrayList<>();
+
+		String sql1 = "SELECT * FROM secprg.transactions inner join secprg.product on secprg.transactions.ProductID = secprg.product.ProductID where  iduser = " + id ;
+
+			
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url,username,password);
+			PreparedStatement st = con.prepareStatement(sql1);
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				Product p = new Product();
+				
+				p.setName(rs.getString(Product.NAME));
+				p.setManufacturer(rs.getString(Product.MANUFACTURER));
+				p.setDescription(rs.getString(Product.DESCRIPTION));
+				p.setIdproducts(rs.getInt(Product.IDPRODUCTS));
+				p.setPrice(rs.getFloat(Product.PRICE));
+				p.setFilename(rs.getString(Product.FILENAME));
+				System.out.println("asdasdas " + p.getFilename());
+
+				productlists.add(p);
+			}
+
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return productlists;
+
+	}
 	
 	public void addTransactions(TransBeana b) {
 		String sql = "insert into secprg.transactions"
